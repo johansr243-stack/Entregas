f254d8a87e6e747917bc11e43c74dbe78b1ce681
Johan Daniel Patiño <johansr243@gmail.com>
2026-04-27 21:47:07 -0500

    fakeapisuma

--- En-clase/04-lunes27/01-fake-api.js
+++ En-clase/04-lunes27/01-fake-api.js
@@ -1,10 +1,21 @@
 const API = 'https://fakeapi.net/products';
 
 async function getProducts() {
-    const response = await fetch(API);
-    const data = await response.json();
+    
+    try {
+        const response = await fetch(API);
+        const data = await response.json();
+        let total = 0;
 
-console.log(data);
+        for (let i = 0; i < data.data.length; i++) {
+            total += data.data[i].price;
+        }
+
+        console.log(total);
+    } catch (error) {
+        console.error('Error al conectarse a la FakeAPI');
+    }
+
 }
 
 getProducts();
