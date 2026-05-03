aca2524547e755f0ce65120c4dcb03955a2283e2
Johan Daniel Patiño <johansr243@gmail.com>
2026-05-03 15:13:31 -0500

    Talleres

---
+++ Talleres/Taller_1/taller_practico-01.js
@@ -0,0 +1,23 @@
+const PI = 3.1416;
+let areaCirculo = PI;
+let radio = Number(prompt('Ingrese el radio del círculo:'));
+areaCirculo *= radio * radio;
+
+alert('El área del círculo es: ' + areaCirculo.toFixed(2));
+
+saludo = 'Hola, ';
+mundo = 'mundo';
+mensaje = saludo + mundo;
+
+alert(mensaje);
+
+mensaje = ('javaScript')
+razon = (' es divertido')
+frase = mensaje.concat(razon)
+
+alert(frase);
+
+operacion = 5 + 7
+message = `El resultado de 5 + 7 es: ${operacion}`
+
+alert(message)
